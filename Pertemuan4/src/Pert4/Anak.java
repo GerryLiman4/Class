9f134763698b2f817af692965c20a68e968f6894
GerryLiman4 <56666756+GerryLiman4@users.noreply.github.com>
2019-10-17 12:49:20 +0700

    Add files via upload

---
+++ Pertemuan4/src/Pert4/Anak.java
@@ -0,0 +1,30 @@
+package Pert4;
+
+public class Anak extends Ayah {
+
+	
+	private String spesial ;
+	public Anak(  String nama, String spesial  , int umur)
+	{
+		super(nama , umur);
+		this.setSpesial(spesial);
+	}
+	
+	public String getSpesial() {
+		return spesial;
+	}
+	public void setSpesial(String spesial) {
+		this.spesial = spesial;
+	}
+	
+	public void out(String special)
+	{
+		System.out.println(getnama());
+		System.out.println(getumur());
+		System.out.println(getSpesial());
+	
+	}
+	
+	
+	
+}
