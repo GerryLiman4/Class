32509c2e677bbe7cb0fda87ce9426cfd516ca926
GerryLiman4 <56666756+GerryLiman4@users.noreply.github.com>
2019-10-17 12:01:42 +0700

    Add files via upload

---
+++ Pertemuan4/src/Pert4/SubKelas.java
@@ -0,0 +1,9 @@
+package Pert4;
+
+public class SubKelas extends SuperKelas {
+
+	public SubKelas()
+	{
+		
+	}
+}
---
+++ Pertemuan4/src/Pert4/SuperKelas.java
@@ -0,0 +1,25 @@
+package Pert4;
+
+public class SuperKelas {
+
+	public static void main(String[] args) {
+		
+		SuperKelas x = new SuperKelas();
+		x.out();
+
+	}
+	
+	public SuperKelas()
+	{
+		super();
+	}
+	
+	public void out()
+	{
+		System.out.println("ok");
+	}
+
+}
+
+
+// public class final -> tidak bisa diturunkan
